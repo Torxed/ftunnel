962519312fc536f9eaa3d2bac9d870024f3b9079
Anton Hvornum <anton.feeds@gmail.com>
2019-05-21 19:36:10 +0200

    Dummy cert and initial code

---
+++ ftunnel.py
@@ -0,0 +1,99 @@
+import sys, ssl, signal, time
+from socket import *
+from select import epoll, EPOLLIN, EPOLLHUP
+
+args = {}
+positionals = []
+for arg in sys.argv[1:]:
+	if '--' == arg[:2]:
+		if '=' in arg:
+			key, val = [x.strip() for x in arg[2:].split('=')]
+		else:
+			key, val = arg[2:], True
+		args[key] = val
+	else:
+		positionals.append(arg)
+
+def sig_handler(signal, frame):
+	s.close()
+	exit(0)
+signal.signal(signal.SIGINT, sig_handler)
+
+class http():
+	def __init__(self, data=b''):
+		self.data = data
+
+	def parse(self):
+		headers, payload = self.data.split(b'\r\n\r\n', 1)
+		return payload
+
+	def build(self):
+		headers = b'POST /{} HTTP/1.1\r\n'.format(time.time())
+		headers += b'Host: hvornum.se\r\n'
+		headers += b'Content-Length: {}\r\n'.format(len(self.data))
+		headers += b'\r\n'
+
+		return headers + self.data
+
+poller = epoll()
+sockets = {}
+
+local, port = args['source'].split(':')
+
+s = socket()
+s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+s.bind((local, int(port)))
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(args['pem'], args['pem'])
+
+sockets = {}
+while 1:
+	for fileno, event in poller.poll(0.25):
+		if fileno == s.fileno():
+			## Accept the client
+			ns, na = s.accept()
+			print(f'{na} has connected.')
+			
+			## Redirect to destination
+			destination = socket()
+			target, port = args['destination'].split(':')
+			destination.connect((target, int(port)))
+
+			## If HTTP is pointed towards destination,
+			## it means we're sending to another stunnel who can
+			## extract the payload.. and it will act as a webserver
+			## - which means we need to wrap the socket as if it's HTTPS traffic.
+			if args['http'] == 'destination':
+				destination = ssl.wrap_socket(destination, server_side=False, cert_reqs=None)
+			## But if the source is HTTP, we need to act as a web server for the source instead.
+			else:
+				ns = context.wrap_socket(ns, server_side=True)
+			print(f'relay to {target} has been established.')
+			
+			## Create the mapping table for source <--> destination
+			sockets[ns.fileno()] = {'sock' : ns, 'addr' : na, 'endpoint' : destination.fileno(), 'type' : 'source'}
+			sockets[destination.fileno()] = {'sock' : ns, 'addr' : na, 'endpoint' : ns.fileno(), 'type' : 'destination'}
+
+			poller.register(ns.fileno(), EPOLLIN|EPOLLHUP)
+			poller.register(destination.fileno(), EPOLLIN|EPOLLHUP)
+
+		elif fileno in sockets:
+			print(f'{sockets[fileno]["addr"]} sent data')
+			data = sockets[fileno]['sock'].recv(8192)
+			if len(data) <= 0:
+				sockets[fileno]['sock'].close()
+				sockets[sockets[fileno]['endpoint']]['sock'].close()
+				poller.unregister(fileno)
+				poller.unregister(sockets[fileno]['endpoint'])
+				del(sockets[sockets[fileno]['endpoint']])
+				del(sockets[fileno])
+
+			if sockets[fileno]['type'] == args['http']:
+				print(f'Extracting payload before sending to endpoint')
+				data = http(data).parse()
+				sockets[sockets[fileno]['endpoint']]['sock'].send(data)
+			else:
+				print(f'Building payload to endpoint')
+				data = http(data).build()
+				sockets[sockets[fileno]['endpoint']]['sock'].send(data)
